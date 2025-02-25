8b8eea36cc4a9aa6e0ebeca426dd61f0e5f6ee36
Billy Brothers <billy.brothers0@gmail.com>
2025-02-24 21:59:39 -0600

    finished worksheet 10-20.

--- usa_names.sql
+++ usa_names.sql
@@ -69,20 +69,84 @@ LIMIT 1
 
 -- 11. What is the most popular name for a girl that starts with the letter X?
 
+SELECT COUNT(name), name
+FROM names
+WHERE name LIKE ('X%') AND gender = 'F'
+GROUP BY name
+ORDER BY count DESC
+
+
+
 -- 12. Write a query to find all (distinct) names that start with a 'Q' but whose second letter is not 'u'.
+
+SELECT DISTINCT name
+FROM names
+WHERE name LIKE ('Q%') AND name NOT LIKE ('%u%')
+
 
 -- 13. Which is the more popular spelling between "Stephen" and "Steven"? Use a single query to answer this question.
 
+SELECT SUM(num_registered) AS total_registered, name
+FROM names
+WHERE name = 'Stephen' OR name = 'Steven'
+GROUP BY name
+
+
 -- 14. Find all names that are "unisex" - that is all names that have been used both for boys and for girls.
+
+SELECT name
+FROM names
+GROUP BY name
+HAVING COUNT(DISTINCT gender) > 1
 
 -- 15. Find all names that have made an appearance in every single year since 1880.
 
+select name
+FROM names
+GROUP BY name
+HAVING COUNT(name) = 138
+
 -- 16. Find all names that have only appeared in one year.
+
+SELECT name
+FROM names
+GROUP BY name
+HAVING COUNT(DISTINCT year) = 1
 
 -- 17. Find all names that only appeared in the 1950s.
 
+SELECT name
+FROM names
+GROUP BY name
+HAVING MIN(Year) >= 1950 AND MAX(YEAR) <= 1959
+
+
 -- 18. Find all names that made their first appearance in the 2010s.
+
+SELECT name
+FROM names
+GROUP BY name
+HAVING MIN(year) >= 2010
+
 
 -- 19. Find the names that have not be used in the longest.
 
+SELECT name
+FROM names
+GROUP BY name
+ORDER BY MAX(year)
+
+
 -- 20. Come up with a question that you would like to answer using this dataset. Then write a query to answer this question.
+
+How long is the longest name(s)? and how long is the shortest(s) names?
+
+SELECT MAX(LENGTH(name)) AS name_length, name
+FROM names
+GROUP BY name
+ORDER BY name_length DESC
+
+SELECT MAX(LENGTH(name)) as name_length, name
+FROM names
+GROUP BY name
+ORDER BY name_length
